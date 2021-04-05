b405ee765d3f120bd122f72b251bedcc94970a8f
Tyler <tylerkershner@gmail.com>
2021-04-05 18:33:48 -0400

    Add fields to appointments seralizer

--- pp/appointments/serializers.py
+++ pp/appointments/serializers.py
@@ -5,3 +5,8 @@ class AppointmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Appointment
         fields = ("id", "datetime", "reason", "new_patient", "contact_phone_number")
+
+        datetime = serializers.DateTimeField()
+        reason = serializers.CharField()
+        new_patient = serializers.BooleanField()
+        contact_phone_number = serializers.CharField()
