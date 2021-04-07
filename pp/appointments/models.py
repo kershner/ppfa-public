f733de764b0cac9f8e4b17800bfeab1e340e7b89
Tyler <tylerkershner@gmail.com>
2021-04-06 21:21:43 -0400

    add str representation/organization

--- pp/appointments/models.py
+++ pp/appointments/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import RegexValidator
 
+
 class Appointment(models.Model):
     ANNUAL_CHECKUP = 'CH'
     GENERAL_CONSULTATION = 'GC'
@@ -16,15 +17,19 @@ class Appointment(models.Model):
         (SCREENING_FOR_DISEASE, 'Screening for Disease'),
         (VACCINATION, 'Vaccination'),
     ]
-
-    phone_number_regex = RegexValidator(regex=r'\([1-9][\d]{2}\)[1-9][\d]{2}-[1-9][\d]{3}', message='Phone numbers must be in the format (xxx)xxx-xxxx')
-
-    datetime = models.DateTimeField(null=False)
     reason = models.CharField(
         max_length=2,
         choices=APPOINTMENT_REASONS,
         default='GC',
         null=False
     )
-    new_patient = models.BooleanField(null=False)
+
+    phone_number_regex = RegexValidator(regex=r'\([1-9][\d]{2}\)[1-9][\d]{2}-[1-9][\d]{3}',
+                                        message='Phone numbers must be in the format (xxx)xxx-xxxx')
     contact_phone_number = models.CharField(validators=[phone_number_regex], max_length=13)
+
+    datetime = models.DateTimeField(null=False)
+    new_patient = models.BooleanField(null=False)
+
+    def __str__(self):
+        return '{} appointment ID: {}'.format(self.get_reason_display(), self.pk)
