48cf9a61e338d58447e21d8d3d27b2f085421104
Tyler <tylerkershner@gmail.com>
2021-04-06 23:25:10 -0400

    add doctors to appointment serializer

--- pp/appointments/serializers.py
+++ pp/appointments/serializers.py
@@ -1,12 +1,15 @@
+from pp.doctors.serializers import DoctorSerializer
 from rest_framework import serializers
 from .models import Appointment
+
 
 class AppointmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Appointment
-        fields = ("id", "datetime", "reason", "new_patient", "contact_phone_number")
+        fields = ('id', 'datetime', 'reason', 'new_patient', 'contact_phone_number', 'doctors')
 
         datetime = serializers.DateTimeField()
         reason = serializers.CharField()
         new_patient = serializers.BooleanField()
         contact_phone_number = serializers.CharField()
+        doctors = DoctorSerializer(many=True)
