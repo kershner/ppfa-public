6ddf22003fcb45611eb4af7c3a423344c704b6f1
Tyler <tylerkershner@gmail.com>
2021-04-06 21:06:46 -0400

    Add doctor/appointments admin for convenience

--- pp/appointments/admin.py
+++ pp/appointments/admin.py
@@ -1,3 +1,13 @@
+from pp.appointments.models import Appointment
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('datetime', 'reason', 'new_patient', 'contact_phone_number')
+    list_display_links = ['datetime', 'reason', 'new_patient', 'contact_phone_number']
+    ordering = ['-id']
+    sortable_by = ['datetime', 'reason', 'new_patient', 'contact_phone_number']
+    save_on_top = True
+    show_full_result_count = True
+    search_fields = ['doctor__name']
---
+++ pp/doctors/admin.py
@@ -0,0 +1,14 @@
+from pp.doctors.models import Doctor
+from django.contrib import admin
+
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'specialization', 'rating')
+    list_display_links = ['name', 'specialization', 'rating']
+    autocomplete_fields = ['appointments']
+    ordering = ['-id']
+    sortable_by = ['name', 'specialization', 'rating']
+    list_filter = ['rating']
+    save_on_top = True
+    show_full_result_count = True
