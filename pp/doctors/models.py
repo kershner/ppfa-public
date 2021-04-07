64134045bd9467dcb7e38fffbcc57309e5a919d5
Tyler <tylerkershner@gmail.com>
2021-04-06 21:05:49 -0400

    add doctor model

---
+++ pp/doctors/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=255)
+
+    PODIATRIST = 'po'
+    CARDIOLOGIST = 'ca'
+    DERMATOLOGIST = 'de'
+    IMMUNOLOGIST = 'im'
+    NEUROLOGIST = 'ne'
+    GYNECOLOGIST = 'gy'
+    SPECIALIZATION_CHOICES = [
+        (PODIATRIST, 'Podiatrist'),
+        (CARDIOLOGIST, 'Cardiologist'),
+        (DERMATOLOGIST, 'Dermatologist'),
+        (IMMUNOLOGIST, 'Immunologist'),
+        (NEUROLOGIST, 'Neurologist'),
+        (GYNECOLOGIST, 'Gynecologist'),
+    ]
+    specialization = models.CharField(
+        max_length=2,
+        choices=SPECIALIZATION_CHOICES,
+        default=PODIATRIST,
+        null=False
+    )
+
+    RATING_CHOICES = [
+        (1, 'Poor'),
+        (2, 'Average'),
+        (3, 'Good'),
+        (4, 'Very Good'),
+        (5, 'Excellent'),
+    ]
+    rating = models.IntegerField(
+        choices=RATING_CHOICES,
+        default=1,
+        null=False
+    )
+
+    appointments = models.ManyToManyField('appointments.Appointment', blank=True)
